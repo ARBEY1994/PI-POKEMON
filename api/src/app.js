17876f08f2506f5431a34a87b0bc1b284050216d
liamgabriel2021 <arbey221@gmail.com>
2022-09-07 15:28:08 -0500

    co

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,12 @@ server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "pi-pokemon-omega.vercel.app"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "pi-pokemon-omega.vercel.app",
+    "pi-pokemon-git-main-arbey1994.vercel.app",
+    "pi-pokemon-arbey1994.vercel.app"
+  ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
