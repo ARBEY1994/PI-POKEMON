70ecb1d2c09ee82468db6213484e61cf4239e2c8
liamgabriel2021 <arbey221@gmail.com>
2022-09-09 23:22:20 -0500

    p

--- api/src/app.js
+++ api/src/app.js
@@ -20,10 +20,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  // res.header(
-  //   "Access-Control-Allow-Origin",
-  //   "https://pi-pokemon-git-main-arbey1994.vercel.app"
-  // );
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
